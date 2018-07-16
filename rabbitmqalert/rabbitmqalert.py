e8cf6b57db398670ccd45e65d7e1e1ea0177c29a
Abdelkader Bouadjadja <abouadjadja@wynd.eu>
2018-07-16 18:35:46 +0200

    Fix incident declared bug

--- rabbitmqalert/rabbitmqalert.py
+++ rabbitmqalert/rabbitmqalert.py
@@ -43,7 +43,7 @@ class RabbitMQAlert:
         if (consumers_connected_min is not None and consumers < consumers_connected_min):
             if False == self._was_consumer_connection_incident_declared:
                 self.send_notification(options, "Alert on queue *%s* | Consumer info : Current count = %d, Expected min count %d" % (queue, consumers, consumers_connected_min))
-                _was_consumer_connected_incident_declared = True
+                self._was_consumer_connected_incident_declared = True
         elif True == self._was_consumer_connection_incident_declared:
             self.send_notification(options, "Incident on queue *%s* finished. Consumer(s) is/are back. Consumer info : Current count = %d, Expected min count %d" % (queue, consumers, consumers_connected_min))
             self._was_consumer_connection_incident_declared = False
